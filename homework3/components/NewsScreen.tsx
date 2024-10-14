5dfb2a585730bd0a7ac74692cf4cb3e7ac00f75d
W1ngshot <kirill-samsonov@bk.ru>
2024-10-14 23:40:23 +0300

    hw3 done

---
+++ homework3/components/NewsScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export function NewsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>
+        News screen
+      </Text>
+    </View>
+  )
+}
